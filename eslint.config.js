723eecaf814239292b1288553b9f973d2cd44fb7
Charan Ravela <charanravela1998@gmail.com>
2025-04-06 00:13:04 -0500

    chore: integrate Prettier with ESLint and enforce consistent formatting rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import jest from 'eslint-plugin-jest';
 import jsdoc from 'eslint-plugin-jsdoc';
 import perfectionist from 'eslint-plugin-perfectionist';
+import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -28,12 +29,13 @@ export default [
       jest,
       jsdoc,
       perfectionist,
+      prettier,
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       security,
       tailwindcss,
-    },    
+    },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
@@ -42,9 +44,10 @@ export default [
       ...tailwindcss.configs.recommended.rules,
       ...perfectionist.configs['recommended-alphabetical'].rules,
       ...jest.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
       'comma-dangle': ['error', 'always-multiline'],
-      'eqeqeq': ['error', 'always'],
-      'indent': ['error', 2, { SwitchCase: 1 }],
+      eqeqeq: ['error', 'always'],
+      indent: ['error', 2, { SwitchCase: 1 }],
       'jsdoc/check-alignment': 'error',
       'jsdoc/check-indentation': 'error',
       'jsdoc/check-param-names': 'error',
@@ -78,13 +81,10 @@ export default [
         { blankLine: 'always', next: '*', prev: 'import' },
         { blankLine: 'any', next: 'import', prev: 'import' },
       ],
-      'quotes': ['error', 'single', { avoidEscape: true }],
-      'react-refresh/only-export-components': [
-        'error',
-        { allowConstantExport: true },
-      ],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
       'react/react-in-jsx-scope': 'off',
-      'semi': ['error', 'always'],
+      semi: ['error', 'always'],
       'space-before-function-paren': ['error', 'never'],
     },
     settings: {
