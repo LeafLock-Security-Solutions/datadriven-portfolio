39b1be23dde47e79f6058c826a421d9ee5d8d87c
Charan Ravela <charanravela1998@gmail.com>
2025-04-10 01:07:06 -0500

    chore: add vite.config.js with ESM-compatible path alias setup
    - Added Vite configuration with React plugin and alias for '@/src'
    - Used `fileURLToPath` and `dirname` to safely resolve paths in ESM

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
 
 // https://vite.dev/config/
 export default defineConfig({
   // Test comment
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(dirName, './src'),
+    },
+  },
 });
